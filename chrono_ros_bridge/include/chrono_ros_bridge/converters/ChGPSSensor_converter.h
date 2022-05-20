cb459b14fb9df6446f6b8ab417d0f1dce0a5c66a
Aaron Young <aryoung5@wisc.edu>
2022-05-19 21:42:32 -0500

    Made GPS converter use strings to preserve precision.

--- chrono_ros_bridge/include/chrono_ros_bridge/converters/ChGPSSensor_converter.h
+++ chrono_ros_bridge/include/chrono_ros_bridge/converters/ChGPSSensor_converter.h
@@ -29,8 +29,14 @@ SerializedMessagePtr ChGPSSensor_converter(chrono::utils::ChJSONReader& reader) 
     std::string type, id;
     reader >> type >> id >> reader.GetObject();
 
+    std::string latitude, longitude, altitude;
+
     sensor_msgs::msg::NavSatFix msg;
-    reader >> msg.latitude >> msg.longitude >> msg.altitude;
+    reader >> latitude >> longitude >> altitude;
+
+    msg.latitude = std::stod(latitude);
+    msg.longitude = std::stod(longitude);
+    msg.altitude = std::stod(altitude);
 
     return Serialize(msg);
 }
